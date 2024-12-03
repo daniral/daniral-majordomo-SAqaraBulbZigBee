f1f3820a8321dbb45330077de5389cb36a6af6fb
daniral <daniralgal@gmail.com>
2024-12-03 17:47:36 +0200

    fix

--- modules/devices/SDimmersCct_brightnessWorkChanged.php
+++ modules/devices/SDimmersCct_brightnessWorkChanged.php
@@ -7,14 +7,16 @@
 
 */
 
+/*
 $brightnessWorkNew = $params['NEW_VALUE'];
 $brightnessWorkOld = $params['OLD_VALUE'];
 $brightnessWorkMin = $this->getProperty('brightnessWorkMin');
 $brightnessWorkMax = $this->getProperty('brightnessWorkMax');
 
-if ($brightnessWorkNew == $brightnessWorkOld || ($brightnessWorkNew < $brightnessWorkMin && $brightnessWorkNew > $brightnessWorkMax)) return;
+if ($brightnessWorkNew == $brightnessWorkOld || $brightnessWorkNew < $brightnessWorkMin || $brightnessWorkNew > $brightnessWorkMax) return;
 
 if ($brightnessWorkMin != $brightnessWorkMax) {
     $brightnessLevel = round(($brightnessWorkNew - $brightnessWorkMin) / (round($brightnessWorkMax - $brightnessWorkMin)) * 100);
-    //$this->setProperty('brightnessLevel', $brightnessLevel);
+    $this->setProperty('brightnessLevel', $brightnessLevel,'','brightnessWorkChanged');
 }
+*/
